4954306f8252ec8cdd841c169c5ecd5ad8c64963
TutankaDev <kenyihq@gmail.com>
2026-04-09 18:19:30 -0500

    fix(models): registrar todos los modelos en __init__

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .persona import Persona
+from .usuario import Usuario
+from .planta import Planta
+from .usuario_planta import UsuarioPlanta
+from .cliente_planta import ClientePlanta
+from .ganadero import Ganadero
+from .registro import Registro

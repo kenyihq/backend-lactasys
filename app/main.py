f47265456752374c27ac270421adc8d9228fccd7
Kenyi Hancco <kenyihq@gmail.com>
2026-04-10 12:32:30 -0500

    feat(api): agregar rutas de analytics

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from app.api.routes import auth
 from app.api.routes import registros
 from app.api.routes import pagos
+from app.api.routes import analytics
 
 app = FastAPI()
 
+app.include_router(analytics.router, prefix="/analytics", tags=["Analytics"])
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(registros.router, prefix="/registros", tags=["Registros"])
 app.include_router(pagos.router, prefix="/pagos", tags=["Pagos"])
